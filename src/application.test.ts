4a030823b4826326658d3146a37cac22a2312f48
Lance Gliser <lance.gliser@torch.ai>
2021-09-01 22:02:27 -0400

    Resolved issues in the middleware and application tests

--- src/application.test.ts
+++ src/application.test.ts
@@ -1,12 +1,11 @@
 import { withResolverContext } from "../tests/graphql.utils";
 
 describe("application", () => {
-  it("should return resolver context for testing", async (done) => {
+  it("should return resolver context for testing", async () => {
     await withResolverContext(async (context) => {
       expect(context.authorization).toBeTruthy();
       expect(context.application).toBeTruthy();
       expect(context.context).toBeTruthy();
-      done();
     });
   });
 });
