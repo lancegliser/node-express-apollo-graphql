9700e4eb914eef086233560b6397610415f2fc50
Lance Gliser <lance.gliser@torch.ai>
2021-09-04 11:57:42 -0400

    Removed the comment we didn't require

--- src/server/events.ts
+++ src/server/events.ts
@@ -2,7 +2,6 @@ import { Application } from "express";
 import { Server } from "http";
 import logger from "./logger";
 
-// This comment should stay in the code
 export const onAppListening = (app: Application, server: Server) => {
   const address = server.address();
   if (!address) {
