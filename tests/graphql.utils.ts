cfcfd7f809fdffc0092d6d990696ac480d9a24a6
Lance Gliser <lance.gliser@torch.ai>
2023-07-16 16:34:21 -0500

    Separated supertest logic from graphql and provided some simple utility methods.

--- tests/graphql.utils.ts
+++ tests/graphql.utils.ts
@@ -1,105 +1,49 @@
-import { Application } from "express";
 import { Response } from "supertest";
-import { getSystemContext, SystemContext } from "../src/components/context";
-import { getApp } from "./application";
-// import { v4 } from "uuid";
+import { RequestContext } from "./utilities";
+import {
+  getAuthorizedSuperTestPostRequest,
+  getCredentialedSuperTestPostRequest,
+} from "./supertest.utils";
+import {rootUri} from "../src/constants";
 
-type TWithSystemContext = (context: SystemContext) => Promise<void>;
-/** Supports internal testing, such as repos */
-export const withSystemContext = async (
-  fn: TWithSystemContext
-): Promise<void> => {
-  const systemContext = await getSystemContext();
-  await fn(systemContext);
-};
+export const postCredentialedGraphQLRequest = async (
+    context: RequestContext,
+    query: string,
+    variables?: Record<string, unknown>
+): Promise<Response> =>
+    getCredentialedSuperTestPostRequest(context, rootUri).send({
+      query,
+      variables,
+    });
 
-// type TWithGraphQLContext = (context: GraphQLContext) => Promise<void>;
-/**
- * Provides the system context with a test users, and an active auth token.
- * If you don't need users, use withSystemContext() for better performance.
- *
- * Typically this would be used when testing utility functions that require
- * resolver context as an argument.
- **/
-// export const withGraphQLContext = async (
-//   fn: TWithGraphQLContext
-// ): Promise<void> => {
-//   await withSystemContext(async (systemContext) => {
-//     const authorization = await getAuthorization();
-//     await fn({
-//       ...systemContext,
-//       users: {
-//         _id: `${UsersSchema.users.name}/tests`,
-//         _key: "tests",
-//         displayName: "Test users",
-//       },
-//       authorization,
-//       logger: logger,
-//       transactionId: v4(),
-//     });
-//   });
-// };
-// const tokenService = new TokenService({
-//   clientId: process.env.TEST_TOKEN_CLIENT_ID,
-//   clientSecret: process.env.TEST_TOKEN_CLIENT_SECRET,
-//   tokenEndpoint: process.env.TEST_TOKEN_ENDPOINT,
-// });
+export const postAuthorizedGraphQLRequest = async (
+    context: RequestContext,
+    query: string,
+    variables?: Record<string, unknown>
+): Promise<Response> =>
+    getAuthorizedSuperTestPostRequest(context, rootUri).send({
+      query,
+      variables,
+    });
 
-/** Defines the shape of data available from outside the application to make requests */
-export interface ResolverContext {
-  context: SystemContext;
-  application: Application;
-  authorization: string;
-}
-type TWithResolverContext = (context: ResolverContext) => Promise<void>;
-let application: Application;
-let context: SystemContext;
-/**
- * Provides a bootstrapped application context and an active auth token for making requests.
- * Includes SystemContext for in case you need to setup or tear down data in your tests.
- **/
-export const withResolverContext = async (
-  fn: TWithResolverContext
-): Promise<void> => {
-  if (!application) {
-    application = await getApp();
-  }
-  if (!context) {
-    context = await getSystemContext();
-  }
-  const authorization = await getAuthorization();
+export const postAnonymousGraphQLRequest = async (
+    context: RequestContext,
+    query: string,
+    variables?: Record<string, unknown>
+): Promise<Response> =>
+    postCredentialedGraphQLRequest(
+        {
+          ...context,
+          credentials: {
+            accessToken: "",
+            refreshToken: "",
+            tokenType: undefined,
+          },
+        },
+        query,
+        variables
+    );
 
-  await fn({
-    context,
-    application,
-    authorization,
-  });
-};
-
-let authorization: string;
-export const getAuthorization = async (): Promise<string> => {
-  if (!authorization) {
-    // const token = await tokenService.getAccessCredentials();
-    // authorization = `${token.token_type} ${token.access_token}`;
-    authorization = `Bearer `;
-  }
-
-  return authorization;
-};
-
-export const expectGraphQLSuccessResponse = (response: Response): void => {
-  if (response.body.error) {
-    throw new Error(response.body.error);
-  }
-  if (response.body.errors?.length > 0) {
-    const error = response.body.errors.slice(0, 1).shift();
-    const message = [
-      ...(error.path || []),
-      error.extensions?.exception?.stacktrace?.join("\r\n") ?? error.message,
-    ]
-      .filter(Boolean)
-      .join(" - ");
-    throw new Error(message);
-  }
-  expect(response.status).toBe(200);
+export const expectUnauthenticatedGraphQLResponse = (response: Response) => {
+  expect(response.body.errors.at(0).extensions.code).toBe("UNAUTHENTICATED");
 };
